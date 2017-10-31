f74ca81cd9d317e53c992f178d176f8004447fe0
최종윤 <@sonzong2>
2017-10-31 21:12:42 +0900

    n

--- src/StringTest.java
+++ src/StringTest.java
@@ -3,7 +3,7 @@ public class StringTest {
 
 	public static void main(String[] args) {
 		
-		String name = "¼¼Á¾´ë¿Õ";
+		String name = "¼¼Á¾´ë¿Õ" ;
 		String cost = "10000";
 		int thisyear = 2017;
 		
